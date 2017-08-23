19bf01ca9f800a70e049c91eba2dd3b4c6ab903d
Michael Sodaro <xtra.zero@gmail.com>
2017-08-23 14:25:37 +0900

    Rename 'Find' className to 'Finder'

--- src/components/Finder.jsx
+++ src/components/Finder.jsx
@@ -4,7 +4,7 @@ import { InputGroup, InputGroupAddon, Input } from 'reactstrap';
 class Finder extends Component {
   render() {
     return (
-      <div className="Find">
+      <div className="Finder">
         <InputGroup className="ISBN">
           <InputGroupAddon>ISBN</InputGroupAddon>
           <Input
