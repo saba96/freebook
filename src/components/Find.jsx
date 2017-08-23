55f64399f18f05f78212b561bdd50bb16d4baea9
Michael Sodaro <xtra.zero@gmail.com>
2017-08-23 11:38:28 +0900

    Add ISBN field

--- src/components/Find.jsx
+++ src/components/Find.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { InputGroup, InputGroupAddon, Input } from 'reactstrap';
 
 class Find extends Component {
   render() {
     return (
       <div className="Find">
-        FIND
+        <InputGroup className="ISBN">
+          <InputGroupAddon>ISBN</InputGroupAddon>
+          <Input placeholder="978-0-123456-47-2"/>
+        </InputGroup>
       </div>
     )
   }
