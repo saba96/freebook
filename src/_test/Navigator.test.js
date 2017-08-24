c0d5bd663e17591244649ec96696b513cb648ea6
Michael Sodaro <xtra.zero@gmail.com>
2017-08-24 11:09:37 +0900

    Further test Navigator component

--- src/_test/Navigator.test.js
+++ src/_test/Navigator.test.js
@@ -17,6 +17,11 @@ describe('Navigator component', () => {
     expect(wrapper.find('.Navigator')).to.have.length(1); 
   });
 
+  it('has two navigation links', () => {
+    const wrapper = shallow(<Navigator />);
+    expect(wrapper.find('NavLink')).to.have.length(2);
+  });
+
   describe('add link', () => {
     it('calls onAddClick', () => {
       // Setup
@@ -26,8 +31,23 @@ describe('Navigator component', () => {
       );
 
       // Exercise
-      console.log(wrapper.find('NavLink').at(0).html());
       wrapper.find('NavLink').at(0).simulate('click', emptyEvent);
+
+      // Assert
+      expect(spy.calledOnce);
+    });
+  });
+
+  describe('find link', () => {
+    it('calls onFindClick', () => {
+      // Setup
+      let spy = sinon.spy();
+      let wrapper = shallow(
+        <Navigator view="find" onFindClick={ spy }/>
+      );
+
+      // Exercise
+      wrapper.find('NavLink').at(1).simulate('click', emptyEvent);
 
       // Assert
       expect(spy.calledOnce);
