733757a3c7d91ce6b6f4bc3b5e57fc201a0247c8
GameKyuubi <xtra.zero@gmail.com>
2017-09-21 21:17:48 +0900

    Serve build folder from root

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
+const routes = require('./routes');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', express.static('build'));
+app.use('/api', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
